43dcc6b110e415dfb72ba5b96cf1f26ef1ad31eb
4332001876 <mzfslhm@163.com>
2024-09-08 21:23:20 +0800

    switch to deepseek api

--- research_town/utils/model_prompting.py
+++ research_town/utils/model_prompting.py
@@ -1,9 +1,12 @@
 import litellm
+from openai import OpenAI
+import os
 from beartype import beartype
 from beartype.typing import Dict, List, Optional
 
 from .error_handler import api_calling_error_exponential_backoff
 
+deepseek_client = OpenAI(api_key=os.getenv("DEEPSEEK_API_KEY"), base_url="https://api.deepseek.com")
 
 @beartype
 @api_calling_error_exponential_backoff(retries=5, base_wait_time=1)
@@ -20,16 +23,32 @@ def model_prompting(
     """
     Select model via router in LiteLLM.
     """
-    completion = litellm.completion(
-        model=llm_model,
-        messages=messages,
-        max_tokens=max_token_num,
-        # for some models, 'n'(The number of chat completion choices ) is not supported.
-        n=return_num,
-        top_p=top_p,
-        temperature=temperature,
-        stream=stream,
-    )
-    content = completion.choices[0].message.content
+    IS_DEEPSEEK = True
+    
+    if not IS_DEEPSEEK:
+        completion = litellm.completion(
+            model=llm_model,
+            messages=messages,
+            max_tokens=max_token_num,
+            # for some models, 'n'(The number of chat completion choices ) is not supported.
+            n=return_num,
+            top_p=top_p,
+            temperature=temperature,
+            stream=stream,
+        )
+        content = completion.choices[0].message.content
+    else:
+        response = deepseek_client.chat.completions.create(
+            model="deepseek-chat",
+            messages=messages,
+            max_tokens=max_token_num,
+            # for some models, 'n'(The number of chat completion choices ) is not supported.
+            n=return_num,
+            top_p=top_p,
+            temperature=temperature,
+            stream=stream,
+        )
+        content = response.choices[0].message.content
+
     content_l = [content]
     return content_l
